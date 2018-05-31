dbb755745f557544f01fe69bd9e97e2160a31818
Paul Sanchez <paxasaval1003@gmail.com>
2018-05-31 10:17:35 -0500

    paxasaval

---
+++ Semana9/src/semana9/Semana9.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package semana9;
+
+/**
+ *
+ * @author User
+ */
+public class Semana9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int[] c=new int[10];
+        System.out.println(c.length);
+        for (int contador=0; contador<c.length; contador++){
+            System.out.println(c[contador]);
+        }
+    }
+    
+}
